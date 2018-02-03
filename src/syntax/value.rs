9e0b3250c09b482ef39137cb851d90b7013a5c9c
Ryan Cumming <etaoins@gmail.com>
2018-02-04 10:59:17 +1100

    Add float to Value
    This isn't parsed yet. This is just for completeness.

--- src/syntax/value.rs
+++ src/syntax/value.rs
@@ -1,10 +1,11 @@
 use syntax::span::Span;
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Value {
     Bool(Span, bool),
     Char(Span, char),
     Int(Span, i64),
+    Float(Span, f64),
     List(Span, Vec<Value>),
     String(Span, String),
     Symbol(Span, String),
