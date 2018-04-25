d570b33a124ec731d7b6ba8227a6b3d0d44c8870
Ryan Cumming <etaoins@gmail.com>
2018-04-25 18:01:14 +1000

    Generalise mono_for_datum in to poly_for_datum
    While data by definition has a mono type, the type inference works on
    poly types. While we could convert the mono to a poly that can get
    expensive with complex types. Instead support creating poly types
    directly.
    
    This uses the Poly/MonoCtx pattern used elsewhere in the type system
    even though we have no state to keep. This is done for consistency; not
    much would be gained by removing support for state.

--- src/ty/datum.rs
+++ src/ty/datum.rs
@@ -1,47 +1,97 @@
 use syntax::datum::Datum;
 use ty;
 
-pub fn mono_for_datum(datum: &Datum) -> ty::Mono {
-    match *datum {
-        Datum::Bool(_, val) => ty::Ty::LitBool(val).into_mono(),
-        Datum::Sym(_, ref val) => ty::Ty::LitSym(val.clone()).into_mono(),
-        Datum::Char(_, _) => ty::Ty::Char.into_mono(),
-        Datum::Int(_, _) => ty::Ty::Int.into_mono(),
-        Datum::Float(_, _) => ty::Ty::Float.into_mono(),
-        Datum::Str(_, _) => ty::Ty::Str.into_mono(),
-        Datum::List(_, ref vs) => vs.iter().rev().fold(
-            ty::Ty::Nil.into_mono(),
-            |tail_mono, fixed_datum| {
-                ty::Ty::Cons(Box::new(mono_for_datum(fixed_datum)), Box::new(tail_mono)).into_mono()
-            },
-        ),
-        Datum::Vec(_, ref vs) => ty::Ty::Vec(vs.iter().map(mono_for_datum).collect()).into_mono(),
-        Datum::Set(_, ref vs) => {
-            // Without function, begin or rest types we should never hit erasure
-            let unified_type = ty::unify::mono_unify_iter(vs.iter().map(mono_for_datum)).unwrap();
-            ty::Ty::Set(Box::new(unified_type)).into_mono()
-        }
-        Datum::Map(_, ref vs) => {
-            let unified_key =
-                ty::unify::mono_unify_iter(vs.iter().map(|&(ref k, _)| mono_for_datum(k))).unwrap();
+trait DatumTyContext<S>
+where
+    S: ty::TyRef,
+{
+    fn unify_ref_iter<I>(&self, members: I) -> S
+    where
+        I: Iterator<Item = S>;
 
-            let unified_value =
-                ty::unify::mono_unify_iter(vs.iter().map(|&(_, ref v)| mono_for_datum(v))).unwrap();
+    fn ref_for_datum(&self, datum: &Datum) -> S
+    where
+        S: ty::TyRef,
+    {
+        match *datum {
+            Datum::Bool(_, val) => S::from_ty(ty::Ty::LitBool(val)),
+            Datum::Sym(_, ref val) => S::from_ty(ty::Ty::LitSym(val.clone())),
+            Datum::Char(_, _) => S::from_ty(ty::Ty::Char),
+            Datum::Int(_, _) => S::from_ty(ty::Ty::Int),
+            Datum::Float(_, _) => S::from_ty(ty::Ty::Float),
+            Datum::Str(_, _) => S::from_ty(ty::Ty::Str),
+            Datum::List(_, ref vs) => vs.iter().rev().fold(
+                S::from_ty(ty::Ty::Nil),
+                |tail_mono, fixed_datum| {
+                    S::from_ty(ty::Ty::Cons(
+                        Box::new(self.ref_for_datum(fixed_datum)),
+                        Box::new(tail_mono),
+                    ))
+                },
+            ),
+            Datum::Vec(_, ref vs) => S::from_ty(ty::Ty::Vec(
+                vs.iter().map(|v| self.ref_for_datum(v)).collect(),
+            )),
+            Datum::Set(_, ref vs) => {
+                // Without function, begin or rest types we should never hit erasure
+                let unified_type = self.unify_ref_iter(vs.iter().map(|v| self.ref_for_datum(v)));
 
-            ty::Ty::Map(Box::new(unified_key), Box::new(unified_value)).into_mono()
+                S::from_ty(ty::Ty::Set(Box::new(unified_type)))
+            }
+            Datum::Map(_, ref vs) => {
+                let unified_key =
+                    self.unify_ref_iter(vs.iter().map(|&(ref k, _)| self.ref_for_datum(k)));
+
+                let unified_value =
+                    self.unify_ref_iter(vs.iter().map(|&(_, ref v)| self.ref_for_datum(v)));
+
+                S::from_ty(ty::Ty::Map(Box::new(unified_key), Box::new(unified_value)))
+            }
         }
     }
+}
+
+struct MonoDatumTyContext {}
+
+impl DatumTyContext<ty::Mono> for MonoDatumTyContext {
+    fn unify_ref_iter<I>(&self, iter: I) -> ty::Mono
+    where
+        I: Iterator<Item = ty::Mono>,
+    {
+        ty::unify::mono_unify_iter(iter).unwrap()
+    }
+}
+
+pub fn mono_for_datum(datum: &Datum) -> ty::Mono {
+    let ctx = MonoDatumTyContext {};
+    ctx.ref_for_datum(datum)
+}
+
+struct PolyDatumTyContext {}
+
+impl DatumTyContext<ty::Poly> for PolyDatumTyContext {
+    fn unify_ref_iter<I>(&self, iter: I) -> ty::Poly
+    where
+        I: Iterator<Item = ty::Poly>,
+    {
+        ty::unify::poly_unify_iter(&[], iter).unwrap()
+    }
+}
+
+pub fn poly_for_datum(datum: &Datum) -> ty::Poly {
+    let ctx = PolyDatumTyContext {};
+    ctx.ref_for_datum(datum)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use hir::poly_for_str;
 
     fn mono_for_str(ty_str: &str) -> ty::Mono {
         use std::collections::HashMap;
-        use hir;
 
-        let poly = hir::poly_for_str(ty_str).unwrap();
+        let poly = poly_for_str(ty_str).unwrap();
         ty::subst::subst(&poly, &HashMap::new()).unwrap()
     }
 
@@ -50,6 +100,7 @@ mod test {
         let datum = datum_from_str(datum_str).unwrap();
 
         assert_eq!(mono_for_str(ty_str), mono_for_datum(&datum));
+        assert_eq!(poly_for_str(ty_str).unwrap(), poly_for_datum(&datum));
     }
 
     #[test]
