1d5c44cb7564fbe9eb26fa0ab1c3635db20d365e
Ryan Cumming <etaoins@gmail.com>
2018-08-19 17:02:33 +1000

    Support more complex lists in (length)

--- compiler/mir/intrinsic/list.rs
+++ compiler/mir/intrinsic/list.rs
@@ -8,19 +8,28 @@ use crate::mir::Value;
 
 pub struct Length {}
 
+impl Length {
+    fn value_len(value: &Value) -> Option<usize> {
+        match value {
+            Value::List(fixed, rest) => {
+                let fixed_len = fixed.len();
+
+                match rest {
+                    Some(rest) => Self::value_len(rest).map(|rest_len| fixed_len + rest_len),
+                    None => Some(fixed_len),
+                }
+            }
+            _ => None,
+        }
+    }
+}
+
 impl Intrinsic for Length {
     fn eval_arg_list(pcx: &mut PartialEvalCtx, mut iter: ListIterator<'_>) -> Option<Value> {
         let single_arg = iter.next_unchecked();
 
-        match single_arg {
-            Value::List(fixed, None) => {
-                let known_length = fixed.len();
-
-                Some(Value::Const(
-                    boxed::Int::new(pcx, known_length as i64).as_any_ref(),
-                ))
-            }
-            _ => None,
-        }
+        Self::value_len(single_arg).map(|known_length| {
+            Value::Const(boxed::Int::new(pcx, known_length as i64).as_any_ref())
+        })
     }
 }
