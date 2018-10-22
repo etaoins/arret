a8c0c70d6fa746fef39d305cc0be5e35ecaaf298
Ryan Cumming <etaoins@gmail.com>
2018-10-22 18:44:50 +1100

    Support direct integer equality
    This uses the new `TypeTagSet` infrastructure to determine if both sides
    are integers

--- compiler/mir/equality.rs
+++ compiler/mir/equality.rs
@@ -14,41 +14,18 @@ use crate::mir::value::to_const::value_to_const;
 use crate::mir::value::Value;
 use crate::ty;
 
-/// Evaluates if two values are equal
-///
-/// If `left_value` and `right_value` are const then a const is returned. Otherwise, a comparison
-/// is built.
-pub fn eval_equality(
+fn runtime_compare(
     ehx: &mut EvalHirCtx,
-    b: &mut Option<Builder>,
+    b: &mut Builder,
     span: Span,
     left_value: &Value,
     right_value: &Value,
-) -> Value {
-    if let Some(const_left) = value_to_const(ehx, left_value) {
-        if let Some(const_right) = value_to_const(ehx, right_value) {
-            let static_result = const_left == const_right;
-            return Value::Const(boxed::Bool::singleton_ref(static_result).as_any_ref());
-        }
-    }
-
-    let some_b = if let Some(some_b) = b {
-        some_b
-    } else {
-        panic!("runtime equality without builder")
-    };
-
-    let left_reg = value_to_reg(
-        ehx,
-        some_b,
-        span,
-        left_value,
-        &abitype::BoxedABIType::Any.into(),
-    );
+) -> RegId {
+    let left_reg = value_to_reg(ehx, b, span, left_value, &abitype::BoxedABIType::Any.into());
 
     let right_reg = value_to_reg(
         ehx,
-        some_b,
+        b,
         span,
         right_value,
         &abitype::BoxedABIType::Any.into(),
@@ -70,7 +47,7 @@ pub fn eval_equality(
         abi,
     });
 
-    let ret_reg = some_b.push_reg(
+    b.push_reg(
         span,
         OpKind::Call,
         CallOp {
@@ -78,11 +55,67 @@ pub fn eval_equality(
             impure: false,
             args: Box::new([left_reg.into(), right_reg.into()]),
         },
-    );
+    )
+}
+
+fn int_compare(
+    ehx: &mut EvalHirCtx,
+    b: &mut Builder,
+    span: Span,
+    left_value: &Value,
+    right_value: &Value,
+) -> RegId {
+    let left_reg = value_to_reg(ehx, b, span, left_value, &abitype::ABIType::Int);
+    let right_reg = value_to_reg(ehx, b, span, right_value, &abitype::ABIType::Int);
+
+    b.push_reg(
+        span,
+        OpKind::IntEqual,
+        BinaryOp {
+            lhs_reg: left_reg.into(),
+            rhs_reg: right_reg.into(),
+        },
+    )
+}
+
+/// Evaluates if two values are equal
+///
+/// If `left_value` and `right_value` are const then a const is returned. Otherwise, a comparison
+/// is built.
+pub fn eval_equality(
+    ehx: &mut EvalHirCtx,
+    b: &mut Option<Builder>,
+    span: Span,
+    left_value: &Value,
+    right_value: &Value,
+) -> Value {
+    use crate::mir::value::types::possible_type_tags_for_value;
+
+    if let Some(const_left) = value_to_const(ehx, left_value) {
+        if let Some(const_right) = value_to_const(ehx, right_value) {
+            let static_result = const_left == const_right;
+            return Value::Const(boxed::Bool::singleton_ref(static_result).as_any_ref());
+        }
+    }
+
+    let b = if let Some(some_b) = b {
+        some_b
+    } else {
+        panic!("runtime equality without builder")
+    };
+
+    let common_type_tags =
+        possible_type_tags_for_value(left_value) & possible_type_tags_for_value(right_value);
+
+    let result_reg = if common_type_tags == boxed::TypeTag::Int.into() {
+        int_compare(ehx, b, span, left_value, right_value)
+    } else {
+        runtime_compare(ehx, b, span, left_value, right_value)
+    };
 
     reg_to_value(
         ehx,
-        ret_reg,
+        result_reg,
         &abitype::ABIType::Bool,
         &ty::Ty::Bool.into_mono(),
     )
