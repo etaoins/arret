a7cb49cb46d9959907c9a19d844ba2b14954b4ab
Ryan Cumming <etaoins@gmail.com>
2018-11-11 16:22:41 +1100

    Consistently export compiler support functions
    - `arret_runtime_equals` wasn't `pub`
    
    - Even though these are exported they also need to be `extern "C"`

--- runtime/compiler_support.rs
+++ runtime/compiler_support.rs
@@ -7,7 +7,7 @@ use crate::task::Task;
 type TaskEntry = extern "C" fn(&mut Task);
 
 #[export_name = "arret_runtime_launch_task"]
-pub fn launch_task(entry: TaskEntry) {
+pub extern "C" fn launch_task(entry: TaskEntry) {
     let mut task = Task::new();
 
     if let Err(err) = panic::catch_unwind(panic::AssertUnwindSafe(|| entry(&mut task))) {
@@ -22,11 +22,11 @@ pub fn launch_task(entry: TaskEntry) {
 }
 
 #[export_name = "arret_runtime_alloc_cells"]
-pub fn alloc_cells(task: &mut Task, count: u32) -> *mut boxed::Any {
+pub extern "C" fn alloc_cells(task: &mut Task, count: u32) -> *mut boxed::Any {
     task.heap_mut().alloc_cells(count as usize)
 }
 
 #[export_name = "arret_runtime_equals"]
-fn equals(lhs: Gc<boxed::Any>, rhs: Gc<boxed::Any>) -> bool {
+pub extern "C" fn equals(lhs: Gc<boxed::Any>, rhs: Gc<boxed::Any>) -> bool {
     lhs == rhs
 }
