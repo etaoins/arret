40096d5bdf2c87d9aff7bd933f73aba7e3c4059d
Ryan Cumming <etaoins@gmail.com>
2019-02-24 20:16:26 +1100

    Complete abi_to_llvm_type
    This isn't immediately useful but not having a catch all makes pattern
    matching exhaustiveness checks work.

--- compiler/codegen/target_gen.rs
+++ compiler/codegen/target_gen.rs
@@ -353,15 +353,13 @@ impl TargetCtx {
             match abi_type {
                 ABIType::Bool => LLVMInt1TypeInContext(self.llx),
                 ABIType::Int => LLVMInt64TypeInContext(self.llx),
+                ABIType::Char => LLVMInt32TypeInContext(self.llx),
                 ABIType::Float => LLVMDoubleTypeInContext(self.llx),
                 ABIType::Boxed(boxed) => self.boxed_abi_to_llvm_ptr_type(boxed),
                 ABIType::Callback(entry_point_abi_type) => {
                     let entry_point_llvm_type =
                         self.callback_entry_point_llvm_type(entry_point_abi_type);
                     self.callback_llvm_type(entry_point_llvm_type)
-                }
-                other => {
-                    unimplemented!("ABI type: {:?}", other);
                 }
             }
         }
