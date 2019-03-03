bd666296598fb789df3ef338eca1575cd91965af
Ryan Cumming <etaoins@gmail.com>
2019-03-03 16:16:01 +1100

    Simplify intrinsics system
    There's really no need for some useless trait when we can easily just
    clal a function. This is probably a Scala/Llambda hangover.

--- compiler/mir/intrinsic/mod.rs
+++ compiler/mir/intrinsic/mod.rs
@@ -3,38 +3,13 @@ mod testing;
 
 use syntax::span::Span;
 
-use crate::mir::intrinsic::list::{Cons, Length};
-use crate::mir::intrinsic::testing::FnOpCategories;
-
 use crate::mir::builder::Builder;
 use crate::mir::error::Result;
 use crate::mir::eval_hir::EvalHirCtx;
-use crate::mir::value::list::ListIterator;
 use crate::mir::Value;
 
-trait Intrinsic {
-    fn eval_arg_list_value(
-        ehx: &mut EvalHirCtx,
-        b: &mut Option<Builder>,
-        span: Span,
-        arg_list_value: &Value,
-    ) -> Result<Option<Value>> {
-        let iter = arg_list_value.list_iter();
-        Self::eval_arg_list(ehx, b, span, iter)
-    }
-
-    fn eval_arg_list(
-        _ehx: &mut EvalHirCtx,
-        _b: &mut Option<Builder>,
-        _span: Span,
-        _iter: ListIterator,
-    ) -> Result<Option<Value>> {
-        panic!("Intrinsic does not implement `eval_arg_list`")
-    }
-}
-
 macro_rules! define_intrinsics {
-    ( $($name:expr => $handler:ident),* ) => {
+    ( $($name:expr => $handler:path),* ) => {
         pub fn try_eval(
             ehx: &mut EvalHirCtx,
             b: &mut Option<Builder>,
@@ -45,7 +20,7 @@ macro_rules! define_intrinsics {
             match intrinsic_name {
                 $(
                     $name => {
-                        $handler::eval_arg_list_value(ehx, b, span, arg_list_value)
+                        $handler(ehx, b, span, arg_list_value)
                     }
                 ),*
                 _ => Ok(None),
@@ -55,7 +30,7 @@ macro_rules! define_intrinsics {
 }
 
 define_intrinsics! {
-    "length" => Length,
-    "cons" => Cons,
-    "fn-op-categories" => FnOpCategories
+    "length" => list::length,
+    "cons" => list::cons,
+    "fn-op-categories" => testing::fn_op_categories
 }
