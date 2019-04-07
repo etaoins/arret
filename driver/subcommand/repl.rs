e5204d9917ce0877dac077d0cd1870228b564ab2
Ryan Cumming <etaoins@gmail.com>
2019-04-07 14:32:11 +1000

    Switch to arret-lang.org
    This is a better name properly hosted in Route53/ACM/S3/Cloudfront
    instead of a dodgy discount registrar.

--- driver/subcommand/repl.rs
+++ driver/subcommand/repl.rs
@@ -244,7 +244,7 @@ impl rustyline::Helper for ArretHelper {}
 ///
 /// This does very little error handling as history is a "nice to have" feature
 fn repl_history_path() -> Option<path::PathBuf> {
-    let project_dirs = directories::ProjectDirs::from("xyz.arret-lang", "", "arret")?;
+    let project_dirs = directories::ProjectDirs::from("org.arret-lang", "", "arret")?;
     let data_dir = project_dirs.data_dir();
 
     fs::create_dir_all(data_dir).ok()?;
