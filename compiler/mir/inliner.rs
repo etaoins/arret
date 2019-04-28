83af1b6eb777dc854474199291ab05acdd1a2ef0
Ryan Cumming <etaoins@gmail.com>
2019-04-28 14:37:22 +1000

    Tweak inline costing
    - We currently include the cost of both branches. However, only one
      branch can be taken so the runtime cost is the average of the
      branches.  On the other hand, the code size (and thus icache
      footprint) is the sum of the branches. Compromise by using the most
      expensive branch.
    
    - Pure functions can be optimised better by both Arret itself and LLVM.
      Give them a two point boost over impure functions.

--- compiler/mir/inliner.rs
+++ compiler/mir/inliner.rs
@@ -70,30 +70,42 @@ type OpCostFactor = f32;
 fn cost_for_op(op: &ops::Op) -> OpCost {
     use crate::mir::ops::OpCategory;
 
-    let inner_cost = if let ops::OpKind::Cond(cond_op) = op.kind() {
-        cost_for_ops(cond_op.true_ops.iter().chain(cond_op.false_ops.iter()))
-    } else {
-        0
+    let category_cost = match op.kind().category() {
+        OpCategory::Unreachable => 0,
+        OpCategory::ConstCastBoxed => 0,
+        OpCategory::ConstReg => 1,
+        OpCategory::RegCast => 1,
+        OpCategory::RegOp => 2,
+        OpCategory::ConstBox => 4,
+        OpCategory::Cond => 5, // Adjusted below to include branches
+        OpCategory::MakeCallback => 5,
+        OpCategory::MemLoad => 5,
+        OpCategory::Ret => 5,
+        OpCategory::Call => 9, // Adjusted below based on the call purity
+        // This is tricky. This could either do a stack allocation (which is cheap) or a heap
+        // allocation (which is very expensive). This depends on the type and escape analysis
+        // in codegen. We need to make use compromise between those two costs here.
+        OpCategory::AllocBoxed => 15,
     };
 
-    inner_cost
-        + match op.kind().category() {
-            OpCategory::Unreachable => 0,
-            OpCategory::ConstCastBoxed => 0,
-            OpCategory::ConstReg => 1,
-            OpCategory::RegCast => 1,
-            OpCategory::RegOp => 2,
-            OpCategory::ConstBox => 4,
-            OpCategory::Cond => 5,
-            OpCategory::MakeCallback => 5,
-            OpCategory::MemLoad => 5,
-            OpCategory::Ret => 5,
-            OpCategory::Call => 10,
-            // This is tricky. This could either do a stack allocation (which is cheap) or a heap
-            // allocation (which is very expensive). This depends on the type and escape analysis
-            // in codegen. We need to make use compromise between those two costs here.
-            OpCategory::AllocBoxed => 15,
+    let op_adjustment = match op.kind() {
+        ops::OpKind::Cond(cond_op) => {
+            // Only one branch can be taken so the runtime cost is the average of the branches. On
+            // the other hand, the code size (and thus icache footprint) is the sum of the
+            // branches. Compromise by using the most expensive branch.
+            std::cmp::max(
+                cost_for_ops(cond_op.true_ops.iter()),
+                cost_for_ops(cond_op.false_ops.iter()),
+            )
         }
+        ops::OpKind::Call(_, call_op) if call_op.impure => {
+            // Impure calls are harder to optimise. Penalise them.
+            2
+        }
+        _ => 0,
+    };
+
+    category_cost + op_adjustment
 }
 
 /// Returns the cost for a sequence of ops
