904279744e12ae137a3ea44a28eb6c5e364a8d89
Ryan Cumming <etaoins@gmail.com>
2020-03-09 16:40:06 +1100

    Git rid of unused workspace name
    The `allow(dead_code)` is easy to forget while this should be much more
    obvious

--- lsp-server/model/workspace.rs
+++ lsp-server/model/workspace.rs
@@ -1,15 +1,10 @@
 #[derive(Debug)]
 pub struct Workspace {
-    name: String,
+    _name: String,
 }
 
 impl Workspace {
     pub fn new(name: String) -> Workspace {
-        Workspace { name }
-    }
-
-    #[allow(dead_code)]
-    pub fn name(&self) -> &str {
-        &self.name
+        Workspace { _name: name }
     }
 }
