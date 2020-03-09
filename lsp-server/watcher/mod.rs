350fde3cd8c18757cb2444d70eda6c32f5ea8c95
Ryan Cumming <etaoins@gmail.com>
2020-03-09 12:28:26 +1100

    Add default impl to DocumentWatcher trait
    It's fine if a watcher doesn't care about all events

--- lsp-server/watcher/mod.rs
+++ lsp-server/watcher/mod.rs
@@ -8,11 +8,11 @@ use crate::model::Document;
 /// Trait for a loosely coupled component that watches document events
 pub trait DocumentWatcher {
     /// Called when a document is opened with the specified initial contents
-    fn did_open(&mut self, url: &lsp_types::Url, document: &Arc<Document>);
+    fn did_open(&mut self, _url: &lsp_types::Url, _document: &Arc<Document>) {}
 
     /// Called when a document has changed with the updated contents
-    fn did_change(&mut self, url: &lsp_types::Url, document: &Arc<Document>);
+    fn did_change(&mut self, _url: &lsp_types::Url, _document: &Arc<Document>) {}
 
     /// Called when a document has closed
-    fn did_close(&mut self, url: &lsp_types::Url);
+    fn did_close(&mut self, _url: &lsp_types::Url) {}
 }
